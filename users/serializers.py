2c552ef3118bdff005b08787dfb76456898e071c
callistachang <rezinomrah@gmail.com>
2020-03-09 21:30:19 +0800

    wow! SlugRelatedField

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,11 @@
 from .views import ModifiedUser
 from rest_framework import serializers
 from skills.serializers import SkillSerializer
+from skills.models import Skill
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    skills = SkillSerializer(many=True)
-
+    skills = serializers.SlugRelatedField(many=True, slug_field="name", queryset=Skill.objects.all())
+    
     class Meta:
         model = ModifiedUser
         fields = ['id', 'email', 'password', 'url', 'skills']
